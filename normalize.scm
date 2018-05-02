6fd79cb4db2a4667dec23c85f9913d544a0cb67b
James P. Torre, IV <torreaiworks01@gmail.com>
2018-05-02 12:33:34 +0000

    cnfo vs dnfo

--- normalize.scm
+++ normalize.scm
@@ -3,6 +3,8 @@
 ;;jpt4
 ;;Reduce arbitrary SL expression to disjunctive normal form
 
+(load "miniKanren-with-symbolic-constraints/mk.scm")
+
 (define (simple-statement ss)
   (conde
    [(symbolo ss) (=/= ss '~) (=/= ss '&) (=/= ss '//)])
@@ -10,16 +12,16 @@
 
 (define (variable s) (simple-statement s))
 
-(define (diso? s)
+(define (dnf-diso? s)
   (fresh (c d)
          (conde
-          [(== `(// ,c ,d) s) (conjo? c) (diso? d)]
-          [(diso? s)])))
+          [(== `(// ,c ,d) s) (dnf-conjo? c) (dnf-diso? d)]
+          [(dnf-conjo? s)])))
 
-(define (conjo? s)
+(define (dnf-conjo? s)
   (fresh (l c)
          (conde
-          [(== `(& ,l ,c) s) (lito? l) (conjo? c)]
+          [(== `(& ,l ,c) s) (lito? l) (dnf-conjo? c)]
           [(lito? s)])))
 
 (define (lito? s)
@@ -28,3 +30,18 @@
           [(== `(~ ,v) s) (variable v)]
           [(variable s)])))
   
+(define (dnfo? s) (dnf-diso? s))
+
+(define (cnf-conjo? s)
+  (fresh (c d)
+         (conde
+          [(== `(& ,d ,c) s) (cnf-diso? d) (cnf-conjo? c)]
+          [(cnf-diso? s)])))
+
+(define (cnf-diso? s)
+  (fresh (l d)
+         (conde
+          [(== `(// ,l ,d) s) (lito? l) (cnf-diso? d)]
+          [(lito? s)])))
+
+(define (cnfo? s) (cnf-conjo? s))
