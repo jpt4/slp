2322bb4cdc121ed374ff2d430b69e7696886cd7a
jpt4-crux <torreaiworks01@gmail.com>
2018-05-06 12:43:43 +0000

    ground-termo clause

--- normalize.scm
+++ normalize.scm
@@ -47,11 +47,28 @@
 (define (cnfo? s) (cnf-conjo? s))
 
 (define (cnfo i o)
-  (fresh (p q r resp resq resr ress)
+  (fresh (p q r resp resq resr resi)
          (conde
+	  [(ground-termo? i) (== i o)]
+	  [(cnfo? i) (== i o)]
+	  [(== `(~ ,p) i) (== `(~ ,resp) o) (cnfo resp p)]
+	  [(== `(// ,p ,q) i) (== `(// ,resp ,resq) o) (cnfo resp p) (cnfo resq q)]
+	  [(== `(& ,p ,q) i) (== `(& ,resp ,resq) o) (cnfo resp p) (cnfo resq q)]
+	  [(== `(~ (~ ,p)) i) (== resp o) (cnfo p resp)]
           [(== `(-> ,p ,q) i) (== `(// (~ ,resp) ,resq) o) (cnfo p resp) (cnfo q resq)]
-          [(== `(~ (// ,p ,q)) i) (== `(& (~ ,p) (~ ,q)) o)]
-          [(== `(~ (& ,p ,q)) i) (== `(// (~ ,p) (~ ,q)) o)]
-          [(== `(~ (~ ,p)) i) (== p o)]
-          [(== `(// ,p (& ,q ,r)) i) (== `(& (// ,p ,q) (// ,p ,r)) o)]
-          )))
+          [(== `(~ (// ,p ,q)) i) (== `(& (~ ,resp) (~ ,resq)) o) (cnfo p resp) (cnfo q resq)]
+          [(== `(~ (& ,p ,q)) i) (== `(// (~ ,resp) (~ ,resq)) o) (cnfo p resp) (cnfo q resq)]
+	  [(== `(// ,p (& ,q ,r)) i) (== `(& (// ,resp ,resq) (// ,resp ,resr)) o) (cnfo p resp) (cnfo q resq) (cnfo r resr)]
+	  )))
+
+(define (ground-termo? t)
+  (fresh (p q)
+	 (conde
+	  [(simple-statement t)]
+	  [(== `(~ ,p) t) (simple-statement p)]
+	  [(== `(// ,p ,q) t) (simple-statement p) (simple-statement q)]
+	  [(== `(& ,p ,q) t) (simple-statement p) (simple-statement q)]
+	  )))
+
+;(~ (~ (// a b))) => (// a b)
+;(~ (~ (// a (// a (& b c))))) => (// a (// a (& b c)))
